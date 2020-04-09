31b0a450a89e77aeeecddc517819f95e4b37c103
pip15612 <63336179+pip15612@users.noreply.github.com>
2020-04-09 15:55:04 +0700

    Update Exercise4_Pavis_C.py

--- Exercise4_Pavis_C.py
+++ Exercise4_Pavis_C.py
@@ -3,7 +3,7 @@ gb = 65
 itcs = 82.5
 cprog = 70
 print("----- Your Score -----")
-print("Foundation English :",fe)
-print("General Business :",gb)
-print("Introduction to Computer System :",itcs)
-print("Computer Programming :",cprog)
+print("Foundation English :",fe,"คะแนน")
+print("General Business :",gb,"คะแนน")
+print("Introduction to Computer System :",itcs,'คะแนน')
+print("Computer Programming :",cprog,"คะแนน")
