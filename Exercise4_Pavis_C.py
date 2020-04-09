40f690028ee70efa8676257ff751013e0ae6ed42
pip15612 <63336179+pip15612@users.noreply.github.com>
2020-04-09 15:50:57 +0700

    Create Exercise4_Pavis_C.py

---
+++ Exercise4_Pavis_C.py
@@ -0,0 +1,9 @@
+fe = 80
+gb = 65
+itcs = 82.5
+cprog = 70
+print("----- Your Score -----")
+print("Foundation English :",fe)
+print("General Business :",gb)
+print("Introduction to Computer System :",itcs)
+print("Computer Programming :",cprog)
